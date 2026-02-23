352a4b658bc1a7fafb6eaadf9ea6fa65daa01ad9
behdad <behdadja@gmail.com>
2026-02-23 13:51:20 +0330

    adding get all profile api

--- backend/centralize/accounts/urls.py
+++ backend/centralize/accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import NonceView, VerifyView, MeView
+from .views import NonceView, VerifyView, MeView, ProfileListView
 
 urlpatterns = [
     path("auth/nonce/", NonceView.as_view()),
     path("auth/verify/", VerifyView.as_view()),
     path("me/", MeView.as_view()),
+    path("me/all/", ProfileListView.as_view()),
 ]
